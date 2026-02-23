00c382fc8571a8bd6f2e0dbcfc42c50f48338be5
Justin Fogo <jfogo@ucsc.edu>
2026-02-23 14:33:24 -0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,33 @@
+// Justin Fogo
+// [Title]
+// An endless runner w/ objects to dodge and collectibles
+// Created: 2/19/26
+
+'use strict'
+
+// game configuration
+const config = {
+    parent: 'myGame',
+    type: Phaser.AUTO,
+    height: 560,
+    width: 720,
+    pixelArt: true,
+    physics: {
+        default: "arcade",
+        arcade: {
+            debug: true
+        }
+    },
+    scene: [],
+};
+
+// define game
+let game = new Phaser.Game(config);
+
+let w = game.config.width;
+let h = game.config.height;
+const textBorder = 64;
+
+let timeScore;
+let cheeseScore;
+let cursors;
---
+++ src/scenes/Load.js
@@ -0,0 +1,7 @@
+class Load extends Phaser.Scene {
+    constructor () {
+        super('loadScene')
+    }
+
+    
+}
